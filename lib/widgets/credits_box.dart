0c51937d4c0eb9a16b1d51fc3e03c3d74dbfb1d0
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-05-29 00:47:10 -0400

    Agrega Easter Egg

--- lib/widgets/credits_box.dart
+++ lib/widgets/credits_box.dart
@@ -46,7 +46,7 @@ class _CreditsBoxState extends State<CreditsBox> {
       child: Column(
         children: <Widget>[
           Container(
-            child: Image.asset('images/usb.png'),
+            child: Image.asset('assets/images/usb.png'),
             margin: const EdgeInsets.symmetric(
               vertical: 16.0,
               horizontal: 64.0
@@ -66,7 +66,7 @@ class _CreditsBoxState extends State<CreditsBox> {
           ),
           Divider(color: Colors.transparent),
           Container(
-            child: Image.asset('images/aray.png'),
+            child: Image.asset('assets/images/aray.png'),
             margin: const EdgeInsets.all(16.0),
           ),
           Text(
@@ -80,6 +80,10 @@ class _CreditsBoxState extends State<CreditsBox> {
           ),
           Text(
             'Tutora: Prof. Ivette C. Martínez.',
+            textAlign: TextAlign.center
+          ),
+          Text(
+            'Agradecimientos a Sthefania Del Valle por la música sacada de su iPod.',
             textAlign: TextAlign.center
           ),
         ],
